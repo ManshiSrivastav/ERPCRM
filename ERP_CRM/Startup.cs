2730156e26e0111c1ab046d69b3c20d8b6775cd1
ManshiSrivastav <mscse8@gmail.com>
2023-01-03 12:13:27 +0530

    first commit

---
+++ ERP_CRM/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(ERP_CRM.Startup))]
+namespace ERP_CRM
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
